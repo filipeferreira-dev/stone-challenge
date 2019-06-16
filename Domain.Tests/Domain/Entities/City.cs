f00d3b66ec06a2a27cd52cd946e57fb141871d8f
Filipe Ferreira <filipeffs10@gmail.com>
2019-06-16 12:37:54 -0300

    adding city model

---
+++ Domain.Tests/Domain/Entities/City.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Domain.Entities
+{
+    public class City
+    {
+        public Guid Key { get; private set; }
+
+        public string Name { get; private set; }
+
+        public string PostalCode { get; private set; }
+
+        public DateTime CreatedOn { get; private set; } = DateTime.Now;
+
+        public City(Guid key, string name, string postalCode)
+        {
+            Key = key;
+            Name = name;
+            PostalCode = postalCode;
+        }
+    }
+}
---
+++ Domain.Tests/Entities/CityTest.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using FluentAssertions;
+using Domain.Entities;
+using System;
+
+namespace Domain.Tests.Entities
+{
+    [TestFixture]
+    public class CityTest
+    {
+        [Test(Description = "On create a new city")]
+        public void OnCreateCity()
+        {
+            var key = Guid.NewGuid();
+            var cityName = Faker.Address.City();
+            var postalCode = Faker.Address.ZipCode();
+
+            var city = new City(key, cityName, postalCode);
+
+            city.CreatedOn.Should().BeOnOrBefore(DateTime.Now);
+            city.Key.Should().Be(key);
+            city.PostalCode.Should().Be(postalCode);
+            city.Name.Should().Be(cityName);
+        }
+
+    }
+}
