6029f51b1c95bf714491129f5449c96a7b80912e
Filipe Ferreira <filipeffs10@gmail.com>
2019-06-20 22:52:05 -0300

    adding tests to domain

--- Domain.Tests/Entities/CityTemperatureTest.cs
+++ Domain.Tests/Entities/CityTemperatureTest.cs
@@ -9,7 +9,7 @@ namespace Domain.Tests.Entities
     public class CityTemperatureTest
     {
         [Test(Description = "Should create a cityTemperature successfully")]
-        public void OnCreateCityTemperature()
+        public void OnCreateCityTemperatureWithCityKeyAndTemperature()
         {
             var cityKey = Guid.NewGuid();
             var temperature = Faker.RandomNumber.Next(0, 30);
@@ -21,6 +21,24 @@ namespace Domain.Tests.Entities
             cityTemperature.IsDeleted.Should().BeFalse();
             cityTemperature.CityKey.Should().Be(cityKey);
             cityTemperature.Temperature.Should().Be(temperature);
+        }
+
+        [Test(Description = "Should create a cityTemperature successfully")]
+        public void OnCreateCityTemperature()
+        {
+            var key = Guid.NewGuid();
+            var cityKey = Guid.NewGuid();
+            var temperature = Faker.RandomNumber.Next(0, 30);
+            var createdOn = DateTime.UtcNow.AddDays(-24);
+
+            var cityTemperature = new CityTemperature(key, cityKey, temperature, createdOn);
+
+            cityTemperature.CreatedOn.Should().Be(createdOn);
+            cityTemperature.DeletedAt.Should().BeNull();
+            cityTemperature.IsDeleted.Should().BeFalse();
+            cityTemperature.CityKey.Should().Be(cityKey);
+            cityTemperature.Temperature.Should().Be(temperature);
+            cityTemperature.Key.Should().Be(key);
         }
     }
 }
