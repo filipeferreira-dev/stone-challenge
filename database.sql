9652db2722262b96f34a30ecdbd6a2186239b485
Filipe Ferreira <filipeffs10@gmail.com>
2019-06-20 21:26:28 -0300

    fixing database script

--- database.sql
+++ database.sql
@@ -1,24 +1,24 @@
-if  not exists (select top 1 1 from sys.databases where [name] = 'st') 
-begin 
-    create database ST;  
-    use ST; 
-    
-    create table City 
-    (
-        [Key] uniqueidentifier primary key,
-        [Name] varchar(500) not null,
-        [PostalCode] varchar(9) not null,
-        [CreatedOn] datetime not null,
-        [DeletedAt] datetime null
-    )
+create database ST
 
-    create table CityTemperature 
-    (
-        [key] uniqueidentifier constraint pk_city_temperature_key primary key,
-        [CityKey] uniqueidentifier not null constraint fk_city_temperature_city_key foreign key references City([key]),
-        [Temperature] int not null,
-        [CreatedOn] datetime not null,
-        [DeletedAt] datetime null
-    )
+go 
+begin
+  use ST;
+end
 
-END
+  create table City 
+  (
+    [Key] uniqueidentifier primary key,
+    [Name] varchar(500) not null,
+    [PostalCode] varchar(9) not null,
+    [CreatedOn] datetime not null,
+    [DeletedAt] datetime null
+  );
+
+  create table CityTemperature 
+  (
+    [key] uniqueidentifier constraint pk_city_temperature_key primary key,
+    [CityKey] uniqueidentifier not null constraint fk_city_temperature_city_key foreign key references City([key]),
+    [Temperature] int not null,
+    [CreatedOn] datetime not null,
+    [DeletedAt] datetime null
+  );
